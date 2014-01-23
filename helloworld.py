733de40c300de173701ed6209a12b3c401cc74ca
MalbonaGenio <pau@badgenius.eu>
2014-01-23 12:43:21 +0100

    more lines

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,19 @@
+print "Hello World!"
+print "Hello Again"
+print "I like typing this"
+print "This is fun"
+print 'Yay! Printing'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
 
-print "Hello World"
+
+
+
+
+
+
+
+
+
+
 
