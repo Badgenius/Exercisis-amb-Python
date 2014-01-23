15bddd7a6ee83d2a92cd2182b97c52277c895cb0
MalbonaGenio <pau@badgenius.eu>
2014-01-23 14:12:43 +0100

    Operacions matematiques

--- Exercisis/matematiques.py
+++ Exercisis/matematiques.py
@@ -1,4 +1,7 @@
+# -*- coding: utf-8 -*-.
+
 # Números i matemàtiques
+
 
 print "Ara contarem pollastres"
 
@@ -10,3 +13,32 @@ print "Gallines", 25 + 30 / 6
 # Imprimim el no Galls i al seu costat 100 - 25 * 3 % 4
 print "Galls", 100 - 25 * 3 % 4
 
+# Imprimim tota la operació sense " "
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# Imprimim la pregunta pero la operació no es calcula
+print "Es veritat que 3 + 2 < 5 - 7?"
+
+# Aqui si que es calcula la opercació, pero no ens retorna un valor numeric.,
+# sino que ens diu si la operació es compleix o no. En aquest cas no es compleix
+# es per aixo que ens imprimeix False.
+print 3 + 2 < 5 - 7
+
+# Imprimim la pregunta i la operació la posem despres separant amb una ','
+print "Quant es 3 + 2?", 3 + 2
+print "Quant es 5 - 7?", 5 - 7
+
+# Quan comparem operacions mirem si es compleix o no. En aquest cas no es compleix
+# es per aixo que abans ens imprimia False.
+print "3 + 2 no es mes petit que 5 - 7, per aixo sortia False."
+
+print "Comprovem-ho."
+
+# Comprovem que 5 es mes gran que -2 amb la operacio >
+print "5 es mes gran que -2?", 5 > -2
+
+# Comprovem que 5 es mes gran o igual que -2 amb el signe >=
+print "5 es mes gran o igual que -2?", 5 >= -2
+
+#Comprovem que 5 es mes petit o igual que -2 amb el signe <=
+print "5 es mes petit o igual que -2?", 5 <= -2
