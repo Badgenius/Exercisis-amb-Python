391c219a13e2ab262b130e740228adc1a5ef4a92
MalbonaGenio <pau@badgenius.eu>
2014-01-23 16:47:36 +0100

    Nou exercici
    Números i matemàtiques pero ara amb decimals inclosos.

---
+++ Exercisis/matematiquesdecimals.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-.
+
+# Números i matemàtiques pero ara amb decimals inclosos.
+# Simplement hem de posar .0 a tot els nombre que formin part de les operacions
+
+
+print "Ara contarem pollastres"
+
+print "Gallines", 25.0 + 30.0 / 6.0
+print "Galls", 100.0 - 25.0 * 3.0 % 4.0
+
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+
+print "Es veritat que 3 + 2 < 5 - 7?"
+
+print 3.0 + 2.0 < 5.0 - 7.0
+print "Quant es 3 + 2?", 3.0 + 2.0
+print "Quant es 5 - 7?", 5.0 - 7.0
+print "3 + 2 no es mes petit que 5 - 7, per aixo sortia False."
+
+
+
+print "Comprovem-ho."
+
+print "5 es mes gran que -2?", 5.0 > -2.0
+print "5 es mes gran o igual que -2?", 5.0 >= -2.0
+print "5 es mes petit o igual que -2?", 5.0 <= -2.0
